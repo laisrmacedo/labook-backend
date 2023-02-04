eccc3ed3d9d66c5051e8256bcdf106b7c92d15ef
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-02-04 23:12:02 +0100

    :sparkles: feat: initial index with UserDatabase

---
+++ src/database/UsersDatabase.ts
@@ -0,0 +1,24 @@
+import { Users } from "../interfaces";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UsersDatabase extends BaseDatabase{
+  //attributes
+  public static TABLE_USERS = "users" //global constant
+
+  //methods
+  public async findUsers(q: string | undefined): Promise<Users[]>{
+    let usersDB
+    if(q){
+      const result = await BaseDatabase
+      .connection(UsersDatabase.TABLE_USERS)
+      .where("name", "LIKE", `%${q}%`)
+      usersDB = result
+    }else{
+      const result = await BaseDatabase
+      .connection(UsersDatabase.TABLE_USERS)
+      usersDB = result
+    }
+    return usersDB
+  }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import console from 'console'
+import { UsersDatabase } from './database/UsersDatabase'
 
 const app = express()
 
@@ -10,3 +11,27 @@ app.use(cors())
 app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
 })
+
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+      const q = req.query.q as string | undefined
+  
+      const usersDataBase = new UsersDatabase()
+      const videosDB = await usersDataBase.findUsers(q)
+
+      res.status(200).send(videosDB)
+  
+    } catch (error) {
+        console.log(error)
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+  }
+)
+
