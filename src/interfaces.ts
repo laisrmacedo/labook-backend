f29691dd614fc8bc050e46847867bbec0b3f4fc9
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-02-04 23:02:10 +0100

    :construction: feat: interface Users

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,3 +2,12 @@ enum Role {
   ADMIN = "Admin",
   NORMAL = "Normal"
 }
+
+export interface Users {
+  id: string,
+  name: string,
+  email: string,
+  password: string,
+  role: string,
+  created_at: string
+}
