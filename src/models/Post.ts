b0180393b9442e4edda8c92f9eda151846b15865
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-02-15 23:53:02 +0100

    :sparkles: feat: method model to DB and business

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,8 +1,10 @@
+import { Posts, PostsDB } from "../interfaces"
+
 export class Post {
   constructor(
     private id: string, 
     private creatorId: string,
-    private contente: string, 
+    private content: string, 
     private likes: number,
     private dislikes: number,
     private createdAt: string,
@@ -16,7 +18,7 @@ export class Post {
     return this.creatorId
   }
   public getContente():string{
-    return this.contente
+    return this.content
   }
   public getLikes():number{
     return this.likes
@@ -30,5 +32,29 @@ export class Post {
   public getUpdatedAt():string{
     return this.updatedAt
   }
+
+  public toDBModel(): PostsDB {
+    return {
+      id: this.id, 
+      creator_id: this.creatorId,
+      content: this.content, 
+      likes: this.likes,
+      dislikes: this.dislikes,
+      created_at: this.createdAt,
+      updated_at: this.updatedAt
+    }
+}
+
+public toBusinessModel(): Posts {
+    return {
+      id: this.id,
+      creatorId: this.creatorId,
+      content: this.content,
+      likes: this.likes,
+      dislikes: this.dislikes,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt
+    }
+}
 
 }
